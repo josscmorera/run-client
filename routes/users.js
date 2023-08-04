e3ae1d47a1e77196a47c69df22e65d7f772e3df4
josscmorera <me@jossmorera.com>
2023-08-04 22:16:42 +0900

    add methods: create, login and validator in user controller

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,13 @@
 var express = require('express');
-const { createUser } = require('../controller/userController');
+const { jwtValidate } = require('../utils/jwtValidate');
+const { checkIfEmpty } = require('../utils/checkIfEmpty');
+const { validateUserData } = require('../utils/validateUserData');
+const { createUser, loginUser, validateUser } = require('../controller/userController');
+
 var router = express.Router();
 
-router.get('/new', createUser);
+router.post('/new', checkIfEmpty, validateUserData,createUser);
+router.post('/login', checkIfEmpty,  loginUser);
+router.get('/validate', jwtValidate, validateUser);
 
 module.exports = router;
