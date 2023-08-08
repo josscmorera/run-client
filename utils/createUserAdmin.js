8f47fd89defeabbfb7c160656246275cb7f837a1
EduDev <caledu@gmail.com>
2023-08-08 19:54:20 +0900

    create an administrator if no user exists

---
+++ utils/createUserAdmin.js
@@ -0,0 +1,29 @@
+const User = require('../model/user');
+const bcrypt = require('bcrypt');
+
+const createUserAdmin = async () => {
+    try {
+        const user = await User.findOne();
+        if(!user){
+
+          const salt = await bcrypt.genSalt(10);
+          const hash = await bcrypt.hash(process.env.USER_ADMIN_PASS, salt);
+          
+            const newUser = new User({
+                firstName: "Admin",
+                lastName: "Movies",
+                email: process.env.USER_ADMIN_EMAIL,
+                password: hash,
+                isAdmin: true,
+            });
+            await newUser.save();
+            console.log("Admin user created");
+        }
+return 
+   } catch (error) {
+        console.log(error);
+        throw new Error("Error creating admin user");
+    }
+}
+
+module.exports = {createUserAdmin};
