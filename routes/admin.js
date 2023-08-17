154d47cab6309d255ef21a8b8e9617bc8591141e
josscmorera <me@jossmorera.com>
2023-08-18 01:43:07 +0900

    change popular movies to latest movies

--- routes/admin.js
+++ routes/admin.js
@@ -1,7 +1,8 @@
 var express = require('express');
-const { createAdmin } = require('../controller/adminController');
+const { createAdmin, loginAdmin } = require('../controller/adminController');
 var router = express.Router();
 
 router.get('/new', createAdmin);
+router.post('/login', loginAdmin);
 
 module.exports = router;
